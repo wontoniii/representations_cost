95e49ca9da3c355ace6880429eb10c621835135d
Francesco Bronzino <wontoniii@gmail.com>
2023-12-20 18:15:32 +0100

    V1

---
+++ run_profile.sh
@@ -0,0 +1,3 @@
+cd /home/traffic-refinery/
+python3 /home/traffic-refinery/scripts/cpu_profile.py -f /home/notebooks/data/ -o /home/notebooks/out/ -c /home/notebooks/trconfig.json -b /home/traffic-refinery/cmd/cpu_profile/cpu_profile.go -l 00
+cd -
