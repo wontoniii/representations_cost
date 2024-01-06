26066af8beb1faabe7714eec7898739f4b317c78
Francesco Bronzino <wontoniii@gmail.com>
2024-01-06 17:28:09 +0100

    Small update

--- run_profile.sh
+++ run_profile.sh
@@ -1,3 +1,3 @@
 cd /home/traffic-refinery/
-python3 /home/traffic-refinery/scripts/cpu_profile.py -f /home/notebooks/data/ -o /home/notebooks/out/ -c /home/notebooks/trconfig.json -b /home/traffic-refinery/cmd/cpu_profile/cpu_profile.go -l 00
+python3 /home/traffic-refinery/scripts/cpu_profile.py -f /home/notebooks/data/ -o /home/notebooks/out/ -c /home/notebooks/trconfig.json -b /home/traffic-refinery/cmd/cpu_profile/cpu_profile.go -l 50
 cd -
